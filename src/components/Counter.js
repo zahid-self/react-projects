59ccede4fb6a30e7a90026bbe77442b1195e1c1c
safhin <mdjahidhasan920@gmail.com>
2022-12-01 22:07:43 +0600

    Initial commit with setup actions,actiontypes,reducers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,46 @@
-const Counter = ({id, count, handleDecrement, handleIncrement }) => {
+import React, { useState } from 'react'
+
+const Counter = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const handleIncrement = () => {
+        setCounter((prevCounter) => {
+            return prevCounter + 1
+        })
+    }
+
+    const handleDecrement = () => {
+        setCounter((prevCounter) => {
+            return prevCounter - 1
+        })
+    }
 
     return (
-        <div class="max-w-md mx-auto mt-10 space-y-5">
-            <div
-                class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-            >
-                <div class="text-2xl font-semibold">{count}</div>
-                <div class="flex space-x-3">
-                    <button
-                        class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                        onClick={() => handleIncrement(id)}
-                    >
-                        Increment
-                    </button>
-                    <button
-                        class="bg-red-400 text-white px-3 py-2 rounded shadow"
-                        onClick={() => handleDecrement(id)}
-                    >
-                        Decrement
-                    </button>
+        <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
+
+            <div class="max-w-md mx-auto mt-10 space-y-5" id="countersParent">
+                <div
+                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+                    <div class="text-2xl font-semibold" id="counter">{ counter }</div>
+                    <div class="flex space-x-3">
+                        <button
+                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                            onClick={handleIncrement}
+                        >
+                            Increment
+                        </button>
+                        <button
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                            onClick={handleDecrement}
+                        >
+                            Decrement
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
